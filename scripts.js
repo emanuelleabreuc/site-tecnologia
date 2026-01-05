8b36126d4e2dc3bb25ed7b2eb4076c5c4f4f4308
emanuelleabreuc <emanuelleslavey@gmail.com>
2026-01-05 20:09:09 -0300

    melhora html e js

--- scripts.js
+++ scripts.js
@@ -9,3 +9,18 @@ const list = document.querySelector('.list');
 let active = 0;
 const total = items.length;
 let timer;
+
+function update(direction) {
+    document.querySelector('.item.active').classList.remove('active'); /*remove a classe active do item que está ativo*/
+    document.querySelector('.dot.active').classList.remove('active'); /*remove a classe active do ponto que está ativo*/
+
+    
+}
+
+prevButton.addEventListener('click', () => { /*toda vez que clicar no prevButton, a função é executada*/
+    update(-1)
+})
+
+nextButton.addEventListener('click', () => { /*toda vez que clicar no nextButton, a função é executada*/
+    update(1) /*sem sinal, é considerado positivo*/
+})
